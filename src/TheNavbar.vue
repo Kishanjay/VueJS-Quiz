a4d3df15b330cee68ecd2cc1eb4d6848f979fe4b
Kishan Nirghin <kishanjay@live.nl>
2020-02-02 16:30:05 +0100

    Add basic navbar component

---
+++ src/TheNavbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <a
+      class="navbar-brand"
+      href="#"
+    >Frontmen Quiz</a>
+
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a
+          class="nav-link"
+          href="#"
+        >Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="#"
+        >Features</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+module.exports = {
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar-nav {
+    display: flex;
+    flex-direction: row;
+}
+.nav-item {
+    margin-left: 16px;
+}
+</style>
