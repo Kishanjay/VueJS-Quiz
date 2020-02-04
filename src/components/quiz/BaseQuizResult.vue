81dd5145fb62989ee9db86104e70437cc3334396
Kishan Nirghin <kishanjay@live.nl>
2020-02-04 22:20:46 +0100

    Extract quiz result as seperate component

---
+++ src/components/quiz/BaseQuizResult.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">
+        Your quiz score: {{ quizResult.score }}%
+      </h5>
+      <div class="list-group">
+        <div
+          v-for="question in quizResult.questions"
+          :key="question.id"
+          class="list-group-item"
+        >
+          <h5>{{ question.question }}</h5>
+          <div
+            v-if="question.user_answer.correct"
+            class="alert alert-success"
+          >
+            {{ question.user_answer.value }}
+          </div>
+          <div
+            v-if="!question.user_answer.correct"
+            class="alert alert-danger"
+          >
+            {{ question.user_answer.value }} <br>
+            <b>Correct answer:</b>
+            {{ question.correct_answer }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    quizResult: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
--- src/pages/FmQuiz.vue
+++ src/pages/FmQuiz.vue
@@ -45,48 +45,21 @@
     </template>
 
     <template v-if="quizFinished">
+      <base-quiz-result :quiz-result="quizResult" />
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">
-            Your quiz score: {{ quizResult.score }}%
-          </h5>
-          <div class="list-group">
-            <div
-              v-for="question in quizResult.questions"
-              :key="question.id"
-              class="list-group-item"
-            >
-              <h5>{{ question.question }}</h5>
-              <div
-                v-if="question.user_answer.correct"
-                class="alert alert-success"
-              >
-                {{ question.user_answer.value }}
-              </div>
-              <div
-                v-if="!question.user_answer.correct"
-                class="alert alert-danger"
-              >
-                {{ question.user_answer.value }} <br>
-                <b>Correct answer:</b>
-                {{ question.correct_answer }}
-              </div>
-            </div>
-          </div>
-          <div class="card-body">
-            <button
-              class="btn btn-primary"
-              @click="resetQuiz"
-            >
-              Try again
-            </button>
-            <router-link
-              to="/scoreboard"
-              class="btn btn-secondary"
-            >
-              Scoreboard
-            </router-link>
-          </div>
+          <button
+            class="btn btn-primary"
+            @click="resetQuiz"
+          >
+            Try again
+          </button>
+          <router-link
+            to="/scoreboard"
+            class="btn btn-secondary"
+          >
+            Scoreboard
+          </router-link>
         </div>
       </div>
     </template>
@@ -100,15 +73,16 @@ import quizRepository from '../repositories/quizRepository';
 import scoreboardRepository from '../repositories/scoreboardRepository';
 
 import BaseAlert from '../components/text/BaseAlert.vue';
-import BaseQuiz from '../components/quiz/BaseQuiz.vue';
 import BaseJumbotron from '../components/text/BaseJumbotron.vue';
-
+import BaseQuiz from '../components/quiz/BaseQuiz.vue';
+import BaseQuizResult from '../components/quiz/BaseQuizResult.vue';
 
 export default {
   components: {
     BaseAlert,
-    BaseQuiz,
     BaseJumbotron,
+    BaseQuiz,
+    BaseQuizResult,
   },
   data() {
     return {
